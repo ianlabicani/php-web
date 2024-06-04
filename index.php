1d2010a7bea09664140c50be45b602245f5715ee
Ian Labicani <labicaniian@gmail.com>
2024-06-04 14:00:40 +0800

    add id to data

--- index.php
+++ index.php
@@ -2,16 +2,22 @@
 // change source of data from static array to database
 $todos = array(
   array(
+    'id' => 1,
     'name' => 'Todo 1',
-    'description' => 'Description 1'
+    'description' => 'Description 1',
+    'status' => 'Incomplete'
   ),
   array(
+    'id' => 2,
     'name' => 'Todo 2',
-    'description' => 'Description 2'
+    'description' => 'Description 2',
+    'status' => 'Complete'
   ),
   array(
+    'id' => 3,
     'name' => 'Todo 3',
-    'description' => 'Description 3'
+    'description' => 'Description 3',
+    'status' => 'Incomplete'
   )
 );
 
@@ -33,19 +39,27 @@ $todos = array(
   </div>
   <div class="card-body">
     <div class="todos-container">
-
     <?php foreach ($todos as $todo) { ?>
       <div class="todo">
-        <p class="todo-name">
-          <?=$todo['name']?>
-        </p>
-        <p class="todo-description">
-          <?=$todo['description']?>
-        </p>
+        <div class="todo-data">
+          <p class="todo-name">
+            <?=$todo['name']?>
+          </p>
+          <p class="todo-description">
+            <?=$todo['description']?>
+          </p>
+        </div>
+        <div class="todo-actions">
+        </div>
+          <form action="index.php">
+            <input type="hidden" name="id" value="<?=$todo['id']?>">
+            <?php if ($todo['status'] === 'Incomplete') { ?>
+            <button class="btn btn-primary" type="submit" name="finish">Finish</button>
+            <?php } ?>
+            <button class="btn btn-danger" type="submit" name="delete">Delete</button>
+          </form>
       </div>
     <?php } ?>
     </div>
   </div>
-</div>
-</body>
 </html>
