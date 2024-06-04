ac1c27ceb5db562aa89c2a91825d6c4c633198ae
Ian Labicani <labicaniian@gmail.com>
2024-06-04 19:47:23 +0800

    include add form and todo component

--- index.php
+++ index.php
@@ -48,32 +48,9 @@ $todos = array(
   }
   ?>
 
-  <form action="tasks/insert.php" method="POST">
-    <div>
-      <label for="">name</label>
-      <input type="text" name="name" id="">
-    </div>
-    <div>
-      <label for="">description</label>
-      <input type="text" name="description" id="">
-    </div>
-    <div>
-      <label for="">Categories</label>
-      <select name="categoriesId[]" id="" multiple>
-        <?php foreach($categories as $category){
-        ?>
-        <option value="<?=$category['id']?>"><?=$category['name']?></option>
-        <?php }?>
-      </select>
-    </div>
-    <div>
-      <input type="submit" value="Enter">
-    </div>
-  </form>
-
-</body>
-
-
+<!-- render add form component -->
+  <?php require "./components/add-form.component.php"?>
+  
 <div class="card todos-card">
   <div class="card-title">
     <h1>Todo Web</h1>
@@ -81,27 +58,11 @@ $todos = array(
   <div class="card-body">
     <div class="todos-container">
       <?php foreach ($todos as $todo) { ?>
-      <div class="todo">
-        <div class="todo-data">
-          <p class="todo-name">
-            <?=$todo['name']?>
-          </p>
-          <p class="todo-description">
-            <?=$todo['description']?>
-          </p>
-        </div>
-        <div class="todo-actions">
-        </div>
-        <form action="index.php">
-          <input type="hidden" name="id" value="<?=$todo['id']?>">
-          <?php if ($todo['status'] === 'Incomplete') { ?>
-          <button class="btn btn-primary" type="submit" name="finish">Finish</button>
-          <?php } ?>
-          <button class="btn btn-danger" type="submit" name="delete">Delete</button>
-        </form>
-      </div>
+        <!-- render todo component -->
+        <?php require "./components/todo.component.php"?>
       <?php } ?>
     </div>
   </div>
+</body>
 
 </html>
