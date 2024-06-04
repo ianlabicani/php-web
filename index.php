15442c9851f8cf34450427a4795fb56265642f0a
Ian Labicani <labicaniian@gmail.com>
2024-06-04 20:47:43 +0800

    chore: Change data source from static array to database

--- index.php
+++ index.php
@@ -1,33 +1,7 @@
-<?php require('core/dbconn.php'); 
-session_start();?>
-
-<?php
-// change source of data from static array to database
-$todos = array(
-  array(
-    'id' => 1,
-    'name' => 'Todo 1',
-    'description' => 'Description 1',
-    'status' => 'Incomplete'
-  ),
-  array(
-    'id' => 2,
-    'name' => 'Todo 2',
-    'description' => 'Description 2',
-    'status' => 'Complete'
-  ),
-  array(
-    'id' => 3,
-    'name' => 'Todo 3',
-    'description' => 'Description 3',
-    'status' => 'Incomplete'
-  )
-);
-
-$sql ="SELECT id, name FROM categories";
-$stmt= $conn->query($sql);
-$categories = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+<?php 
+require "./core/dbconn.php";
+require "./tasks/fetch.php";
+$todos = fetchTodos($conn);
 ?>
 
 
@@ -42,12 +16,6 @@ $categories = $stmt->fetchAll(PDO::FETCH_ASSOC);
 </head>
 
 <body>
-  <?php if(isset($_SESSION['success'])){
-    echo $_SESSION['success'];
-    unset($_SESSION['success']);
-  }
-  ?>
-
 <!-- render add form component -->
   <?php require "./components/add-form.component.php"?>
   
